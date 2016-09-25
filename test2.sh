b54d15c449fa10ce084ca6564bf3165fb836dfea
tingchenlrx <ting.chen.lrx@gmail.com>
2016-09-24 23:00:04 -0400

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 set -x
-(echo $1
+echo $1
 echo $2
 echo $PWD
 
@@ -12,4 +12,5 @@ cd $2
 if [ -f test.txt ]; then exit 5; fi;
 if [ ! -f test.txt ]; then exit 6; fi;
 if [ ! -d hello ]; then mkdir hello; fi;
-exit 7;)  2>&1 | tee -a "$2/log_$1.txt"
+exit 7
+#)  2>&1 | tee -a "$2/log_$1.txt"
