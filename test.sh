dcfcd33e6745b242075a0cf366f94634041b14ea
tingchenlrx <ting.chen.lrx@gmail.com>
2016-09-24 21:35:58 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -x
-#(
+(
 echo $1
 echo $2
 echo $PWD
 
 cd $2
 
-if [ -f "$2/log_$1.txt" ]; then rm "$2/log_$1.txt"; fi;
+#if [ -f "$2/log_$1.txt" ]; then rm "$2/log_$1.txt"; fi;
 if [ -f test.txt ]; then exit 5; fi;
-#)  2>&1 | tee -a "$2/log_$1.txt"
-mkdir hello
+if [! -f test.txt ]; then exit 6; fi;
+mkdir hello)  2>&1 | tee -a "$2/log_$1.txt"
